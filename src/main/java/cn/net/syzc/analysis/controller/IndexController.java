fdee75f52e49d23da4e3a120444ae3a4b16515fa
zong <1016767058@qq.com>
2020-02-07 12:29:45 +0800

    login test

--- src/main/java/cn/net/syzc/analysis/controller/IndexController.java
+++ src/main/java/cn/net/syzc/analysis/controller/IndexController.java
@@ -1,7 +1,10 @@
 package cn.net.syzc.analysis.controller;
 
+import cn.net.syzc.analysis.kit.BaseResponse;
+import cn.net.syzc.analysis.kit.ResultCodeEnum;
 import cn.net.syzc.analysis.service.IndexService;
 import com.jfinal.core.Controller;
+import com.jfinal.kit.StrKit;
 
 @SuppressWarnings("unused")
 public class IndexController extends Controller {
@@ -12,9 +15,15 @@ public class IndexController extends Controller {
     }
 
     public void login() {
+        BaseResponse baseResponse = new BaseResponse();
         String username = getPara("username");
         String password = getPara("password");
-
+        if (!StrKit.isBlank(username) && !StrKit.isBlank(password)) {
+            baseResponse = indexService.login(username, password);
+        } else {
+            baseResponse.setResult(ResultCodeEnum.PARA_NUM_ERROR);
+        }
+        renderJson(baseResponse);
     }
 
 }
--- src/main/java/cn/net/syzc/analysis/service/IndexService.java
+++ src/main/java/cn/net/syzc/analysis/service/IndexService.java
@@ -1,39 +1,27 @@
 package cn.net.syzc.analysis.service;
 
 import cn.net.syzc.analysis.kit.BaseResponse;
+import cn.net.syzc.analysis.kit.ResultCodeEnum;
 import cn.net.syzc.analysis.model.Task;
 import cn.net.syzc.analysis.model.User;
-import cn.net.syzc.analysis.model._MappingKit;
-import com.jfinal.kit.PropKit;
-import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
-import com.jfinal.plugin.activerecord.dialect.MysqlDialect;
-import com.jfinal.plugin.druid.DruidPlugin;
-import org.junit.jupiter.api.Test;
 
 public class IndexService {
     private static User userDao = new User().dao();
     private static Task taskDao = new Task().dao();
 
-    static {
-        PropKit.use("config.properties");
-        DruidPlugin dbPlugin = new DruidPlugin(PropKit.get("jdbcUrl"), PropKit.get("user"), PropKit.get("password"));
-        ActiveRecordPlugin arp = new ActiveRecordPlugin(dbPlugin);
-        arp.setShowSql(PropKit.getBoolean("devMode"));
-        arp.setDialect(new MysqlDialect());
-        _MappingKit.mapping(arp);
-        dbPlugin.start();
-        arp.start();
-    }
-
-    @Test
-    public void login() {
+    public BaseResponse login(String username, String password) {
         System.out.println("test");
         BaseResponse response = new BaseResponse();
-        String username = "test";
-        String password = "a";
-        User user = userDao.findFirst("select * from user where UserName = ? and UserPassword = ?", username, password);
-        System.out.println("user = " + user);
-        System.out.println(user == null);
-        // return response;
+        User user = userDao.findFirst("select * from user where UserName = ? ", username);
+        if (user != null){
+            if (user.getUserPassword().equals(password)){
+                response.setResult(ResultCodeEnum.LOGIN_SUCCESS);
+            }else {
+                response.setResult(ResultCodeEnum.LOGIN_ERROR);
+            }
+        } else {
+            response.setResult(ResultCodeEnum.NO_EXIST_USER);
+        }
+        return response;
     }
 }
