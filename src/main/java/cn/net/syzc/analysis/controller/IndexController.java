b694b114e4b23cc0077ec318106ca183321f18b4
wangzhengyang <zy_wao@163.com>
2020-02-07 22:02:16 +0800

    Get task list and display in that table

--- src/main/java/cn/net/syzc/analysis/controller/IndexController.java
+++ src/main/java/cn/net/syzc/analysis/controller/IndexController.java
@@ -2,10 +2,13 @@ package cn.net.syzc.analysis.controller;
 
 import cn.net.syzc.analysis.kit.BaseResponse;
 import cn.net.syzc.analysis.kit.ResultCodeEnum;
+import cn.net.syzc.analysis.model.Task;
 import cn.net.syzc.analysis.model.User;
 import cn.net.syzc.analysis.service.IndexService;
 import com.jfinal.core.Controller;
 import com.jfinal.kit.StrKit;
+
+import java.util.List;
 
 @SuppressWarnings("unused")
 public class IndexController extends Controller {
@@ -47,6 +50,26 @@ public class IndexController extends Controller {
             // clear the session
             removeSessionAttr("user_id");
             getSession().invalidate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            baseResponse.setResult(ResultCodeEnum.UNKNOWN_ERROR);
+        } finally {
+            renderJson(baseResponse);
+        }
+    }
+
+    /**
+     *
+     */
+    public void getTaskList() {
+        BaseResponse baseResponse = new BaseResponse();
+        try {
+            String UserId = getPara("UserId");
+            if (!StrKit.isBlank(UserId)) {
+                List<Task> taskList = indexService.getTaskList(UserId);
+            } else {
+                baseResponse.setResult(ResultCodeEnum.PARA_NUM_ERROR);
+            }
         } catch (Exception e) {
             e.printStackTrace();
             baseResponse.setResult(ResultCodeEnum.UNKNOWN_ERROR);
