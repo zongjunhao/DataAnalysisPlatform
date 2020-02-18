5db2283820fd3f5b4d4c36f23a5a8f85cc4d81cf
wangzhengyang <zy_wao@163.com>
2020-02-18 21:27:24 +0800

    Clicking the browser's back button does not return

--- src/main/webapp/assets/js/index.js
+++ src/main/webapp/assets/js/index.js
@@ -1,24 +1,14 @@
-// $(document).ready(function () {
-//     $.ajax({
-//         type: "POST",
-//         url: "judgeLogin",
-//         datatype: 'json',
-//         data: {
+$(document).ready(function (e) {
+    var counter = 0;
+    if (window.history && window.history.pushState) {
+        $(window).on('popstate', function () {
+            window.history.pushState('forward', null, '#');
+            window.history.forward(1);
+            // alert("不可回退");  //如果需在弹框就有它
+            // self.location="index.html"; //如查需要跳转页面就用它
+        });
+    }
 
-//         }, // 发送数据
-//         error: function () {
-//             layer.msg('request failed', {
-//                 time: 1000
-//             });
-//         },
-//         success: function (jsonobj) {
-//             if (jsonobj.resultCode === "1008") {/* not login */
-//                 layer.msg(jsonobj.resultDesc, {
-//                     time: 1000
-//                 });
-
-//                 window.location.href = "login.html";
-//             }
-//         },
-//     });
-// });
+    window.history.pushState('forward', null, '#'); //在IE中必须得有这两行
+    window.history.forward(1);
+});
